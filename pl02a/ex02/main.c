d8880c4051990a8b45a65fa2f730743d8832cc33
Mark956 <1140956@isep.ipp.pt>
2015-09-22 14:36:44 +0000

    Resolução do ex03 ficha2

--- pl02a/ex02/main.c
+++ pl02a/ex02/main.c
@@ -15,16 +15,16 @@ int lerNum(int vec[]){
 	int x, i;
 	a = &vec[0];
 	
-	for (i = 0; i<=19; i++){
+	for (i = 0; i<=3; i++){
 		printf("Número: ");
 		scanf("%d", &x);
 		*a = x;
 		a++;
 	}
 	
-	//verificação do vector.
-	for (i = 0; i<=19; i++){
-		printf("%d ; ", vec[i]);
+	//verificação do vector
+	for (i = 0; i<=3; i++){
+		printf("%d", vec[i]);
 	}
 	
 }
