c55d85609818ecab7a10bd73b0dddcdf13752173
Mark956 <1140956@isep.ipp.pt>
2015-09-23 16:36:58 +0000

    Modificação ex04 ficha02

--- pl02a/ex04/main.c
+++ pl02a/ex04/main.c
@@ -1,35 +1,33 @@
 #include <stdio.h>
 
-int ex4(int *end);
+int ex4(char *end);
 
 int main(){
 	
 	char string[255];
-	string[0] = "a";
-	string[1] = "B";
-	string[2] = "K";
-	string[3] = "l";
-	string[4] = "x";
-	
-	
-	int *a;
+	char *a;
 	a = string;
+	
+	string[0] = 'a';
+	string[1] = 'B';
+	string[2] = 'K';
+	string[3] = 'l';
+	string[4] = 'x';
 	
 	ex4(a);
 	
 }
 
-int ex4(int *end){
-	
-	for (i = 0; i < 5; i++){
-		
+int ex4(char *end){
+	int i;
+	for (i = 0; i < 5; i++){	
 		if(*end>64 && *end<91){
 			*end = *end +32;
-			printf("%d", *end);
+			printf("%c", *end);
+		}else{
+			printf("%c", *end);	
 		}
-		
 		end++;
-		
 	}
 	
 	
