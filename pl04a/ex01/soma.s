6281427352616b4169e4218119e68883f4f72b3e
PeFerreira98 <1140953@isep.ipp.pt>
2015-10-07 17:14:06 +0100

    Resolução ex01 ficha03

---
+++ pl04a/ex01/soma.s
@@ -0,0 +1,16 @@
+.section .data
+
+.global op1
+.global op2
+.global res
+
+.section .text
+
+.global soma	#void soma(void)
+
+soma:
+	movl op1, %ebx		#coloca o valor do op1 em ebx
+	movl op2, %eax		#coloca o valor de op2 em eax
+	addl %ebx, %eax		#soma o valor armazenado no ebx com eax e guarda o valor da soma em eax
+	movl %eax, res		#copia para a variavel res
+	ret
