cf25eeb88672c4d494b0712b8b3ef8583c28c856
Mark956 <1140956@isep.ipp.pt>
2015-10-13 13:56:33 +0000

    Resolução ex02

---
+++ pl04b/ex02/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "soma.h"
+
+int op1=0, op2=0;
+
+int main(void){
+	int res = 0;
+	printf("Valor op1:");
+	scanf("%d", &op1);
+	printf("Valor op2:");
+	scanf("%d", &op2);
+
+	res = soma();
+
+	printf("soma = %d:0x%x\n", res, res);
+
+	return 0;
+}
---
+++ pl04b/ex02/soma.s
@@ -0,0 +1,14 @@
+.section .data
+
+.global op1
+.global op2
+
+.section .text
+
+.global soma	
+
+soma:
+	movl op1, %ebx		#coloca o valor do op1 em ebx
+	movl op2, %eax		#coloca o valor de op2 em eax
+	addl %ebx, %eax		#soma o valor armazenado no ebx com eax e guarda o valor da soma em eax
+	ret
