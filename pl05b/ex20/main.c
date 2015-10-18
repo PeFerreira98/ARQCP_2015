dcce13299db1fb0aedaec0f60c818b264fa6b87a
Mark956 <1140956@isep.ipp.pt>
2015-10-18 21:30:20 +0000

    Fix ex20

--- pl05b/ex20/main.c
+++ pl05b/ex20/main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include "soma.h"
 
-int i=10, j = 6;
+int i=0, j = 0;
 
 int main(void){
 	int h = 0;
-	h = f3();
 	
-	printf("f: %d", h);
+	printf("Valor de i: ");
+	scanf("%d", &i);
+	
+	printf("Valor de i: ");
+	scanf("%d", &j);
+	h = f();
+	printf("f: %d\n", h);
+	h = f2();
+	printf("f: %d\n", h);
+	h = f3();
+	printf("f: %d\n", h);
+	h = f4();
+	printf("f: %d\n", h);
 
 	return 0;
 }
--- pl05b/ex20/soma.s
+++ pl05b/ex20/soma.s
@@ -9,6 +9,7 @@
 .global f	
 .global f2
 .global f3
+.global f4
 
 fequal:
 	subl %ecx, %eax
@@ -33,14 +34,14 @@ f2:
 	movl i, %eax
 	movl j, %ecx
 	
-	cmpl %eax, %ecx
+	cmpl %ecx, %eax
 	jge f2ge
 	addl $1, %ecx
 	imull %ecx, %eax
 	ret
 	
 f3ge:
-	imull %ecx, %eax
+	addl %ecx, %eax
 	movl $0, %edx
 	movl i, %ebx
 	addl $1, %ebx
@@ -64,9 +65,27 @@ f3:
 	
 	div  %ecx
 	
-	
+	ret
+
+f4le:
+	movl i, %eax
+	imull $2, %eax
 	
 	ret
 	
+f4:
+	movl i, %eax
+	movl j, %ecx
+	addl %ecx, %eax
+	movl $0, %edx
+	
+	cmpl $10, %eax
+	jl f4le
+	
+	movl j, %eax
+	movl $3, %ecx
+	div %ecx
+	
+	ret
 	
 
