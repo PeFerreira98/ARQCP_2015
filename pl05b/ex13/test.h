04f9c7c06d864922c354555fd1e0260bdbbc72ec
PeFerreira98 <1140953@isep.ipp.pt>
2015-10-18 22:55:57 +0100

    Resolução ex13 (incomplete)

---
+++ pl05b/ex13/test.h
@@ -0,0 +1,1 @@
+long int test (void);
---
+++ pl05b/ex13/test.s
@@ -0,0 +1,24 @@
+.section .data
+
+.global A, B, C, D
+
+.section .text
+
+.global test	
+
+test:
+	movl A, %ebx
+	movl B, %ecx
+	movl C, %esi
+	movl D, %edi
+	movl $0, %eax
+
+	imull %esi, %ecx
+
+	addl %ecx, %eax
+	div %edi
+
+	addl %edi, %eax
+	ret
+
+
