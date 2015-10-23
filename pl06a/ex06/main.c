6de3cbe24038f71946cf81e76cce5330843f4880
PeFerreira98 <1140953@isep.ipp.pt>
2015-10-23 13:04:23 +0100

    Resolução ex06 (complete)

---
+++ pl06a/ex06/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "soma.h"
+
+char *ptr1, *ptr2, *ptr3;
+
+int main(void){
+	char str1[40]= "Life is made of Examples";
+	char str2[40]= ", choose to Be one!";
+	char str3[80];
+	int i;
+	
+	ptr1 = str1;
+	ptr2 = str2;
+	ptr3 = str3;
+
+	i = soma();
+
+	for(i=0; i<strlen(str3); i++){
+		printf("%c", str3[i]);
+	}
+
+	printf("\n");
+
+	return 0;
+}
---
+++ pl06a/ex06/soma.s
@@ -0,0 +1,43 @@
+.section .data
+
+.global ptr1, ptr2, ptr3
+
+.section .text
+
+.global soma
+
+soma:
+	movl ptr1, %esi
+	movl ptr2, %edi
+	movl ptr3, %ecx
+	movl $0, %eax
+	
+
+loop_1:
+	movb (%esi), %bl
+	movb %bl, (%ecx)	
+
+	cmpb $0, (%esi)
+	je loop_2		#salta para o loop2
+
+	addl $1, %esi
+	addl $1, %ecx
+	jmp loop_1
+
+
+loop_2:
+	movb (%edi), %bl
+	movb %bl, (%ecx)	
+
+	cmpb $0, (%edi)
+	je fim			#salta para o fim
+
+	addl $1, %edi
+	addl $1, %ecx
+	jmp loop_2
+	
+
+fim:	
+	ret
+	
+
