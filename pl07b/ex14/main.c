4b9d3e0ddfdae11fcf2cf00d54561af20686e848
PeFerreira98 <1140953@isep.ipp.pt>
2015-11-01 18:30:17 +0000

    Resolução ex14 (incomplete) (falta de reg)

---
+++ pl07b/ex14/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include "soma.h"
+
+short int num;
+short int *ptrvec1, *ptrvec2;
+int *ptrvec3;
+
+int main(void){
+	short int vec1[10] = {10,10,4,3,5,10,7,1,22,8};
+	short int vec2[10] = {8,10,5,0,0,0,7,0,8,10};
+	int vec3[10] = {0,0,0,0,0,0,0,0,0,0};
+	int res = 0, i = 0;	
+
+	ptrvec1 = vec1;
+	ptrvec2 = vec2;
+	ptrvec3 = vec3;
+	
+	num = 10;
+
+	res = soma();
+
+	printf("res  = %d\n", res);
+	printf("vec1 = ");
+	
+	for(i=0; i<num; i++){
+		printf("%hi ", vec1[i]);
+	}
+	printf("\n");
+	printf("vec2 = ");
+	for(i=0; i<num; i++){
+		printf("%hi ", vec2[i]);
+	}
+	printf("\n");
+	printf("vec3 = ");
+	for(i=0; i<res; i++){
+		printf("%d ", vec3[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
---
+++ pl07b/ex14/soma.s
@@ -0,0 +1,59 @@
+.section .data
+
+.global ptrvec1, ptrvec2, ptrvec3, num
+
+.section .text
+
+.global soma
+
+soma:
+	movl ptrvec1, %esi
+	movl ptrvec2, %edi
+	movl ptrvec3, %ebx
+	movl num, %ecx
+	movl $0, %eax
+	movl $0, %edx
+	
+loop:
+	cmpl $0, %ecx		#caso contador 0,
+	je fim				#salta para o fim	
+
+	pushl %ecx			#guarda o 1ºcontador na stack (supostamente)
+
+	movw (%esi), %dx	
+	movl num, %ecx		#inicia o 2ºcontador
+	
+loop2:
+	cmpl $0, %ecx		#caso contador 0,
+	je not_found		#sig nr não foi encontrado no 2 array
+
+	cmpw %dx, (%edi)
+	je add_elem
+
+	addl $2, %edi		#prox posição do 2º array
+	decl %ecx
+	jmp loop2
+
+
+add_elem:
+	movw %dx, (%ebx)	#Guarda nr repetido no 3º array
+	incl %eax
+	
+	addl $2, %esi		#prox posição do 1º array
+	addl $4, %ebx		#prox posição do 2º array
+	
+	popl %ecx			#ecx volta a ter o valor do 1ºcontador (supostamente)
+	decl %ecx
+	
+	jmp loop
+	
+not_found:
+	addl $2, %esi		#prox posição do 1º array
+
+	popl %ecx			#ecx volta a ter o valor do 1ºcontador (supostamente)
+	decl %ecx		
+	
+	jmp loop	
+	
+fim:	
+	ret
