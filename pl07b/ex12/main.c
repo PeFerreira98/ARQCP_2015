6f53c681c9e4fe33c0c9189c511f20f569c6e147
PeFerreira98 <1140953@isep.ipp.pt>
2015-11-04 17:08:00 +0000

    Resolução ex02 (complete)

--- pl07b/ex12/main.c
+++ pl07b/ex12/main.c
@@ -6,21 +6,21 @@ long long int *ptrvec;
 int num = 10;
 
 int main(void){
-	long long int vec[10] = {43000000000,2,999,3,5,1000,7,1,65539,8};
+	long long int vec[10] = {43000000000,2,999,3,-1002,1000,7,1,65539,8};
 	int i = 0;	
 	int count = 0;
 	
 	ptrvec = vec;
 
 	for(i=0; i<num; i++){
-		printf("%llu ", vec[i]);
+		printf("%lli ", vec[i]);
 	}
 	printf("\n");
 	
 	count = vec_corta();
 	
 	for(i=0; i<num; i++){
-		printf("%llu ", vec[i]);
+		printf("%lli ", vec[i]);
 	}
 	printf("\nModificações: %d\n", count);
 	
--- pl08a/ex02/main.c
+++ pl08a/ex02/main.c
@@ -2,43 +2,15 @@
 #include <string.h>
 #include "soma.h"
 
-long int num, aux;
-long int *ptrvec1, *ptrvec2, *ptrvec3;
-
 int main(void){
-	long int vec1[10] = {65340,2,4,3,5,10,7,1,65551,8};
-	long int vec2[10] = {2,20,0,4,65340,69,7,98,65551,88};
-	long int vec3[10] = {0,0,0,0,0,0,0,0,0,0};
-	int res = 0, i = 0;	
-
-	ptrvec1 = vec1;
-	ptrvec2 = vec2;
-	ptrvec3 = vec3;
+	int res = 0, n = 0;	
 	
-	num = 10;
-	aux = num;
+	printf("insira n: ");
+	scanf("%d", &n);
 
-	res = comuns();
+	res = somatorio_n(n);
 
 	printf("res  = %d\n", res);
 	
-	printf("vec1 = ");
-	for(i=0; i<num; i++){
-		printf("%ld ", vec1[i]);
-	}
-	printf("\n");
-	
-	printf("vec2 = ");
-	for(i=0; i<num; i++){
-		printf("%ld ", vec2[i]);
-	}
-	printf("\n");
-	
-	printf("vec3 = ");
-	for(i=0; i<res; i++){
-		printf("%ld ", vec3[i]);
-	}
-	printf("\n");
-
 	return 0;
 }
