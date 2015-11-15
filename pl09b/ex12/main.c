b158ca82846c506d374feac01f8ef9f0ded47494
PeFerreira98 <1140953@isep.ipp.pt>
2015-11-15 05:30:21 +0000

    ex12 C part finished (complete)

--- pl09b/ex12/main.c
+++ pl09b/ex12/main.c
@@ -5,14 +5,15 @@
 
 int main(void){
 	int *vec;
-	int value = 15, pos = 4, res = 0;
+	int value = 15, pos = 5, res = 0;
 	//Tendo em conta que a primeira posição é 0
+	//Warning ao mudar o value, mudar tambem em baixo (antes da func em assembly)
 	vec = &value;
 	
 	res = activa_bit_c(vec, pos);
 	printf("res = %d, colocando 1b na pos %d, o nr apresenta o valor %d\n", res, pos, *vec);
 
-	res = 0;
+	value = 15, res = 0;
 	res = activa_bit_s(vec, pos);
 	printf("res = %d, colocando 1b na pos %d, o nr apresenta o valor %d\n", res, pos, *vec);
 
--- pl09b/ex12/soma2.c
+++ pl09b/ex12/soma2.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-int activa_bit_c (int *vec, int pos){
-	return -1;
+unsigned int c_rol(const unsigned int value, int shift) {
+    if ((shift &= sizeof(value)*8 - 1) == 0) return value;
+    return (value << shift) | (value >> (sizeof(value)*8 - shift));
 }
+
+unsigned int c_ror(const unsigned int value, int shift) {
+    if ((shift &= sizeof(value)*8 - 1) == 0) return value;
+    return (value >> shift) | (value << (sizeof(value)*8 - shift));
+}
+
+int activa_bit_c (int *vec, int pos){
+	int old = *vec;
+	int new = *vec;
+	int MASK = 0x00000001;
+	
+	new = c_ror(new, pos);
+	
+	new = new | MASK;
+	
+	new = c_rol(new, pos);
+	
+	if(old == new) return 0;
+	
+	*vec = new;
+	return 1;
+}
+
